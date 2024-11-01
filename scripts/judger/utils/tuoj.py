32d9114428bfc9e5341c204bf5270f34dc5e0262
yaoxi-std <yaoxi20061225@126.com>
2024-11-01 21:29:13 +0800

    Update ext_info with short status

--- scripts/judger/utils/tuoj.py
+++ scripts/judger/utils/tuoj.py
@@ -2,7 +2,7 @@ import logging
 import requests
 
 from .report import *
-from .result import Status, JudgeResult
+from .result import Status, CaseResult
 
 TUOJ_STATUS = {
     Status.Waiting: "Waiting",
@@ -21,28 +21,43 @@ TUOJ_STATUS = {
     Status.SystemError: "System Error",
 }
 
+SHORT_STATUS = {
+    Status.Waiting: "?",
+    Status.Compiling: "?",
+    Status.Running: "?",
+    Status.Accepted: "*",
+    Status.Skipped: "-",
+    Status.PartiallyCorrect: "p",
+    Status.OutputLimitExceeded: "o",
+    Status.TimeLimitExceeded: "t",
+    Status.MemoryLimitExceeded: "m",
+    Status.WrongAnswer: "x",
+    Status.RuntimeError: "!",
+    Status.FileError: "!",
+    Status.SystemError: "?",
+}
+
+def parse_ext_info(cases: list[CaseResult]):
+  return "".join([f"{SHORT_STATUS[case.status]}" for case in cases])
+
 
 def report_judge_result(report, judged):
   judge_task = current_judge_task()
   judge_result = current_judge_result()
   results = {}
-  
+
   logging.debug(judge_result)
-  
+
   if judge_result.status in [Status.SystemError, Status.CompileError]:
     results["Compilation"] = {
         "status": "Compilation Error",
-        "ext_info": {
-            "Compilation Info": judge_result.message
-        },
+        "ext_info": judge_result.message,
         "is_final": judged
     }
   elif judge_result.status not in [Status.Waiting, Status.Compiling]:
     results["Compilation"] = {
         "status": "Compilation Success",
-        "ext_info": {
-            "Compilation Info": judge_result.message
-        },
+        "ext_info": judge_result.message,
         "is_final": judged
     }
 
@@ -52,9 +67,7 @@ def report_judge_result(report, judged):
         "time": subtask.max_time * 1000,
         "memory": subtask.max_memory * 1024,
         "score": subtask.score,
-        "ext_info": {
-            "Judge Info": "\n".join([f"Case {i + 1}: {TUOJ_STATUS[case.status]} {case.score}" for i, case in enumerate(subtask.cases)])
-        },
+        "ext_info": parse_ext_info(subtask.cases),
         "is_final": judged
     }
 
@@ -63,8 +76,7 @@ def report_judge_result(report, judged):
       "token": config.WEB_TOKEN,
       "results": results
   }
-  
+
   logging.debug(frm)
-  
+
   requests.post(config.REPORT_URL, json=frm)
-  

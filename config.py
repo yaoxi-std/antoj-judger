f225eb7c747dd71043cc98514eab552c63fba3c1
yaoxi-std <yaoxi20061225@126.com>
2024-11-01 17:19:13 +0800

    Put logs to stderr

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
+import sys
 import logging
 from urllib.parse import urljoin
 
 DEBUG = os.getenv("DEBUG", "false") == "true"
 
-logging.basicConfig(level=logging.DEBUG if DEBUG else logging.INFO)
+logging.basicConfig(level=logging.DEBUG if DEBUG else logging.INFO,
+                    handlers=[logging.StreamHandler(sys.stdout)])
 
 WEB_HOST = os.getenv("WEB_HOST", "http://localhost:8010")
 WEB_TOKEN = os.getenv("WEB_TOKEN", "farfarfaraway")
@@ -17,8 +19,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TMP_PATH = os.getenv("TMP_DIR", os.path.join(BASE_DIR, "tmp"))
 DATA_PATH = os.getenv("DATA_PATH", os.path.join(BASE_DIR, "data"))
 
-TESTLIB_PATH = os.getenv("TESTLIB_PATH", os.path.join(BASE_DIR, "include", "testlib.h"))
-CHECKER_PATH = os.getenv("CHECKER_PATH", os.path.join(BASE_DIR, "scripts", "checker"))
+TESTLIB_PATH = os.getenv("TESTLIB_PATH", os.path.join(
+    BASE_DIR, "include", "testlib.h"))
+CHECKER_PATH = os.getenv(
+    "CHECKER_PATH", os.path.join(BASE_DIR, "scripts", "checker"))
 
 SUBMIT_PATH = os.getenv(
     "SUBMIT_PATH", os.path.join(TMP_PATH, "submit"))
@@ -29,7 +33,8 @@ HOST_SANDBOX_DATA_PATH = os.getenv("HOST_SANDBOX_DATA_PATH", SANDBOX_DATA_PATH)
 JUDGER_IDLE_TIME = float(os.getenv("JUDGER_IDLE_TIME", 5))
 JUDGER_REPORT_TIME = float(os.getenv("JUDGER_REPORT_TIME", 1))
 
-ALLOW_CUSTOM_JUDGER = os.getenv("ALLOW_CUSTOM_JUDGER", "false") == "true" or DEBUG
+ALLOW_CUSTOM_JUDGER = os.getenv(
+    "ALLOW_CUSTOM_JUDGER", "false") == "true" or DEBUG
 
 if not os.path.exists(TMP_PATH):
   os.makedirs(TMP_PATH)
